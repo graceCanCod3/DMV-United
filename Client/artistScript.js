8ea56a9325af3ad3e9fd8e1abc5d8bb28d9ab7b3
graceCancod3 <gracem@graces-iMac.local>
2024-06-06 20:34:59 -0400

    general updates made

--- Client/artistScript.js
+++ Client/artistScript.js
@@ -1,6 +1,4 @@
 const artistEndpoint = `http://localhost:3003/artist`
-// const genXartistButton = document.getElementById('')
-// const genXartist = document.querySelector("")
 const artists = `http://localhost:3003/artist`
 
 window.onload = async (event) => {
@@ -15,18 +13,20 @@ try {
     const artistInfo = await response.json();
     console.log(artistInfo)
 
-    // Filter movies by target year
+    
+    // linkElement.href = `display-products.html?id=${product._id}`;
+
     // const gogoArtist = artistInfo.filter(artist => artist.genreId === targetId);
     const artistContainer = document.getElementById('artist-container');
             artistInfo.forEach(artist => {
                 const artistElement = document.createElement('div');
                 artistElement.innerHTML = `
-                    <img src="${artist.image}" alt="artist image" width='250' height='250'>
-                    <h2>Artist: ${artist.name}</h2>
+                    <img class="nextPage" src="${artist.image}" alt="artist image" width='250' height='250'>
+                    <h2 class="nextPage"><a href="song.html?id=${artist._id}">Artist: ${artist.name}</a></h2>
                     <p>DOB: ${artist.dateOfBirth}</p>
                     <p>Genre: ${artist.genre}</p>
                     <p>Bio: ${artist.bio}</p>
-                    <a href="${artist.url}" target="_blank">Arist Website</a>
+                    <a href="${artist.url}" target="_blank">Artist Website</a>
                     <p></p>
                 `;
                 artistContainer.appendChild(artistElement);
